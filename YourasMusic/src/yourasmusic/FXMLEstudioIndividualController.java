f52b22ae605731937d3b5a79748e82abe7452f28
Pedro Mata Rodrigues <pmmatarodrigues@gmail.com>
2018-06-15 01:49:27 +0100

    Add Estudio Ocupado warning

--- YourasMusic/src/yourasmusic/FXMLEstudioIndividualController.java
+++ YourasMusic/src/yourasmusic/FXMLEstudioIndividualController.java
@@ -10,13 +10,16 @@ import java.net.URL;
 import java.sql.Date;
 import java.time.ZoneId;
 import java.util.List;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import org.hibernate.Criteria;
@@ -63,14 +66,35 @@ public class FXMLEstudioIndividualController implements Initializable {
         // --- Recebe a data do DatePicker
         java.util.Date dataReserva = Date.from(dtDataReserva.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant());
        
+        // --- Receber o utilizador com login iniciado
         Utilizador user = IniciarSessaoController.userLogin;
         
+        List<Reserva> reservas = session.createCriteria(Reserva.class).list();
+        
+        for(Reserva r : reservas){
+            if(r.getDataReserva().equals(dataReserva)){
+                mensagemPopup("Data já ocupada", "ocupado", Alert.AlertType.ERROR);
+            }
+        }
+        
         Reserva reserva;
+        // ---- Registar a reserva
         reserva = new Reserva(dataReserva, user, estudio, 'N', 'N');              
         session.beginTransaction();
         session.save(reserva);
         session.getTransaction().commit(); 
         session.close();
     }
+    
+    
+    private boolean mensagemPopup(String title, String texto, Alert.AlertType tipo){
+        Alert alert = new Alert(tipo);
+        alert.setTitle(title);
+        alert.setHeaderText(texto);
+        
+        Optional<ButtonType> option = alert.showAndWait();
+        return option.get() == ButtonType.CANCEL;
+    } 
+    
     
 }
