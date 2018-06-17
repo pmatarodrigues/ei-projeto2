63f4ef1652d384f35de882c7e8ab8ddf4f6de935
Pedro Mata Rodrigues <pmmatarodrigues@gmail.com>
2018-06-17 23:31:02 +0100

    Add Album cover

--- YourasMusic/src/yourasmusic/FXMLAlbumIndividualController.java
+++ YourasMusic/src/yourasmusic/FXMLAlbumIndividualController.java
@@ -2,8 +2,14 @@
 package yourasmusic;
 
 import classes.Album;
+import classes.Artista;
+import java.awt.Toolkit;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.sql.Blob;
+import java.sql.SQLException;
+import java.util.List;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -13,24 +19,70 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class FXMLAlbumIndividualController implements Initializable {
 
     @FXML Label nomeAlbum;
     @FXML Label anoAlbum;
+    @FXML Label lblCapa;
     
-    Album artistaAtual;
+    Album albumAtual;
+    Artista artistaAlbum;
+    
+    Session session;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         
     }    
     
-    void initData(Album albumClicado) {
-        this.artistaAtual = albumClicado;
+    void initData(Album albumClicado) throws SQLException {
+        this.albumAtual = albumClicado;
         nomeAlbum.setText(albumClicado.getNome());
-        anoAlbum.setText(albumClicado.getAno());
+        anoAlbum.setText(albumClicado.getAno());                
+        
+        if(albumAtual.getCapa() != null){
+            carregarCapa();            
+        }
+        else{
+            lblCapa.setText("Capa nao disponível");
+        }
     }
 
+    private void carregarCapa() throws SQLException{
+        ImageView capa;
+        
+        // atribuir imagem a Image
+        Image imagem = new Image(albumAtual.getCapa().getBinaryStream());
+        capa = new ImageView(imagem);
+        capa.setPreserveRatio(false);
+        capa.setFitHeight(550);
+        capa.setFitWidth(550);
+        lblCapa.setText("");
+        // -- "Escrever" a imagem na label
+        lblCapa.setGraphic(capa);;         
+
+    }
+    
+    private String verificarArtista(){
+        
+        session = hibernate.HibernateUtil.getSessionFactory().openSession();
+        
+        List<Artista> artistas = session.createCriteria(Artista.class).list();
+        
+        for(Artista a : artistas){
+            if(a.equals(albumAtual.getArtista())){
+                artistaAlbum = a;
+            }
+        }
+        
+        session.close();
+        
+        return artistaAlbum.getNomeArtista();
+    }
     
 }
--- YourasMusic/src/yourasmusic/FXMLArtistaIndividualController.java
+++ YourasMusic/src/yourasmusic/FXMLArtistaIndividualController.java
@@ -3,22 +3,41 @@ package yourasmusic;
 
 import classes.Album;
 import classes.Artista;
+import classes.Musica;
+import classes.Reserva;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.TilePane;
+import javafx.scene.text.TextAlignment;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.LogicalExpression;
+import org.hibernate.criterion.Restrictions;
 
 
 public class FXMLArtistaIndividualController implements Initializable {
 
     @FXML Label nomeAlbum;
     @FXML Label anoAlbum;
+    @FXML TilePane tileMusicaArtista;
     
-    Artista artistaAtual;
+    Artista artistaAtual;  
 
+    Session session;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+            carregarMusicasArtista();
     }    
     
     void initData(Artista artistaClicado) {
@@ -26,5 +45,33 @@ public class FXMLArtistaIndividualController implements Initializable {
         nomeAlbum.setText(artistaClicado.getNomeArtista());
         anoAlbum.setText(artistaClicado.getNacionalidade());
     }   
+    
+    
+    private void carregarMusicasArtista(){
+        session = hibernate.HibernateUtil.getSessionFactory().openSession();
+
+        Criteria cr = session.createCriteria(Musica.class);
+        List<Musica> musicas = cr.list();
+        
+        Criterion mus = Restrictions.eq("utilizador", artistaAtual);
+ 
+        LogicalExpression musi = Restrictions.and(mus, mus);
+        cr.add(musi);
+                
+        for(Musica m : musicas){
+            Button btnMusicas = new Button();
+            btnMusicas.setId("button_tile");
+            btnMusicas.setMinSize(800, 50);
+            btnMusicas.setMaxSize(800, 50);
+            btnMusicas.setTextAlignment(TextAlignment.LEFT);
+            btnMusicas.setText(m.getNome().toString());
+            tileMusicaArtista.setAlignment(Pos.TOP_CENTER);
+            tileMusicaArtista.setVgap(10);
+            tileMusicaArtista.getChildren().add(btnMusicas);
+            
+        }   
+        
+        session.close();
+    }
     
 }
